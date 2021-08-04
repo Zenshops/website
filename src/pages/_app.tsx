ad5c62de27d798283b9c86e00a7788b13197b583
Arnab Tarwani <arnab.tarwani@gmail.com>
2021-08-05 00:08:08 +0100

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+import { DefaultSeo } from 'next-seo';
+import '../styles.css';
+
+function App({ Component, pageProps }: AppProps) {
+
+  return (
+      <ThemeProvider storageKey="preferred-theme" attribute="class">
+        <DefaultSeo
+          defaultTitle="Zenshops"
+          titleTemplate="%s | Zenshops"
+          description="Welcome to Zenshops!"
+        />
+        <Component {...pageProps} />
+      </ThemeProvider>
+  );
+}
+
+export default App;
