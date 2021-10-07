6b8a491500b860c842da89ee6e5b2afd75e70941
Arnab Tarwani <arnab.tarwani@gmail.com>
2021-10-07 02:20:41 +0100

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="flex justify-center text-center mt-52">
+        <div className="flex justify-center text-center lg:mt-52 sm:m-28">
             © Copyright {new Date().getFullYear()} Zenshops Inc. Made with ♥︎ in Ireland.
         </div>
     );
